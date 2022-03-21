e5f72f63ec111ecc212600b5e2f172aee5bf593d
murat kara <mail.muratkara@gmail.com>
2022-03-21 04:43:37 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  console.log('ready!');
+  $('.navbar_wraper').load('../components/navbar.html');
+  $('.footer_wrapper').load('../components/footer.html');
+  $('.link-tree_wrapper').load('../components/link-tree.html');
+  $('.headerline_wrapper').load('../components/headerline.html');
+  $('.img-banner_wrapper').load('../components/img-banner.html');
+});
